26056df1ef04536338dc65dd4d5c60451c73bebb
Pavel Didenko <win4ester2016@yandex.ru>
2023-01-11 21:37:25 +0300

    feat: add proficiency, projects and education sections

---
+++ JS/index.js
@@ -0,0 +1,49 @@
+import projects from '../JSON/projects.json' assert {type: 'json'};
+
+function generateProjectRow(num){
+    const name = projects[num].name;
+    const link = projects[num].link;
+    const technologies = projects[num].technologies;
+    const publishment = projects[num].publishment;
+
+    const tableBody = document.querySelector('.projects__tbody');
+    const tableRow = document.createElement("tr");
+    tableRow.className = "projects__tr";
+
+    const nameCell = document.createElement("td");
+    nameCell.className = "projects__td";
+    nameCell.innerHTML = `${name}`;
+    tableRow.appendChild(nameCell);
+
+    const gitHubCell = document.createElement("td");
+    gitHubCell.className = "projects__td";
+    gitHubCell.innerHTML = `<a href="${link}">${name}<a>`;
+    tableRow.appendChild(gitHubCell);
+
+    const technologiesCell = document.createElement("td");
+    technologiesCell.className = "projects__td";
+    technologiesCell.innerHTML = `${technologies}`;
+    tableRow.appendChild(technologiesCell);
+
+    const publishmentCell = document.createElement('td');
+    publishmentCell.className = "projects__td";
+
+    if(publishment === 'none'){
+      publishmentCell.innerHTML = `${publishment}`;
+    }else{
+      publishmentCell.innerHTML = `<a href="${publishment}">published</a>`
+    }
+
+    tableRow.appendChild(publishmentCell);
+
+    tableBody.appendChild(tableRow);
+}
+
+
+function fillProjectTable(){
+  projects.reverse().map((value, index) => {
+    generateProjectRow(index);
+  })
+}
+
+fillProjectTable();
