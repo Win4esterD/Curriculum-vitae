10b38a998e4fddb4338896ee3d855bd258ed64ad
Pavel Didenko <win4ester2016@yandex.ru>
2023-01-12 20:32:36 +0300

    refactor: dispose of 'magic numbers' in BurgerMenu class

--- JS/burger.js
+++ JS/burger.js
@@ -1,28 +1,31 @@
-class burgerMenu{
-  constructor(menuSelector, headerSelector, navSelector, mainSelector){
+class BurgerMenu{
+  constructor(menuSelector, headerSelector, navSelector, mainSelector, displayProperty, minHeight, maxHeight){
       this.menu = document.querySelector(menuSelector);
       this.header = document.querySelector(headerSelector);
       this.navigation = document.querySelector(navSelector);
       this.main = document.querySelector(mainSelector);
+      this.minHeight = minHeight;
+      this.maxHeight = maxHeight;
+      this.display = displayProperty;
 }
 
 activate(){
   this.menu.addEventListener('click', () => {
     if(window.getComputedStyle(this.navigation).display === 'none'){
-      this.header.style.height = '50px';
-      this.navigation.style.display = 'flex';
+      this.header.style.height = `${this.maxHeight}`;
+      this.navigation.style.display = `${this.display}`;
       setTimeout(() => {
         this.navigation.style.opacity = '1';
       }, 500)
     }else{
       this.navigation.style.opacity = '0';
       this.navigation.style.display = 'none';
-      this.header.style.height = "20px";
+      this.header.style.height = `${this.minHeight}`;
     }
   });
 
   this.main.addEventListener('click', () => {
-    this.header.style.height = "20px";
+    this.header.style.height = `${this.minHeight}`;
     this.navigation.style.opacity = '0';
     this.navigation.style.display = 'none';
     setTimeout(() => this.navigation.style.display = 'none', 500)
@@ -30,5 +33,5 @@ activate(){
 }
 }
 
-const burger = new burgerMenu('.burger-menu', '.header', '.nav__ul', 'main');
+const burger = new BurgerMenu('.burger-menu', '.header', '.nav__ul', 'main', 'flex', '20px', '50px');
 burger.activate();
