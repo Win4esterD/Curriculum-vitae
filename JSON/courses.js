5158e132d4be1529b2978cc27cd69f8113d59987
Pavel Didenko <patriot2006_06@mail.ru>
2023-04-08 17:51:59 +0300

    feat: add certificate of accomplishment of PHP/MySQL course

--- JSON/courses.js
+++ JSON/courses.js
@@ -145,10 +145,18 @@ const courses = [
   },
   {
     "id": 19,
+    "name": "PHP/MySQL",
+    "provider": "https://fructcode.com/",
+    "link": "https://fructcode.com/ru/courses/php-and-mysql/",
+    "year": "2023",
+    "certificate": "https://fructcode.com/ru/certificates/42142ef3f176b62a29cbb613e36fd001/en/"
+  },
+  {
+    "id": 20,
     "name": "JavaScript/Front-end",
     "provider": "RS-School",
     "link": "https://rollingscopes.com/",
     "year": "2023",
     "certificate": "No"
-  }
+  },
 ]
